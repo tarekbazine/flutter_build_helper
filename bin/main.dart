5ad6634b324d5b545561c30ba5560b677d218a0f
tarek bazine <et_bazine@esi.dz>
2021-04-03 17:18:05 +0200

    init

---
+++ bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_build_helper/flutter_build_helper.dart';
+
+void main(List<String> arguments) {
+  // print(introMessage('0.9.0'));
+
+  build();
+}
---
+++ lib/flutter_build_helper.dart
@@ -0,0 +1,5 @@
+void build() {
+  buildForAndroid();
+}
+
+void buildForAndroid() {}
